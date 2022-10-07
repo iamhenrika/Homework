857700302f68f8edd37b2224c4dce70634d8c057
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2022-10-07 13:51:53 -0400

    Update 100722 - JavaScript Functions Lab.js

--- 100722 - JavaScript Functions Lab.js
+++ 100722 - JavaScript Functions Lab.js
@@ -27,11 +27,11 @@ function maxOfTwoNumbers(x, y) {
   // or more "elegantly" using the fantastic ternary expression!
   // return  x >= y ? x : y;
 }
-console.log(maxOfTwoNumbers(3, 9))
+console.log(maxOfTwoNumbers(3, 9));
 
 // 2. Define a function, as a function expression, maxOfThree that takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
 
-function maxOfThree(x, y, z) {
+const maxOfThree = function (x, y, z) {
   if (x >= y && x >= z) {
     return x;
   } else if (y >= x && y >= z) {
@@ -40,12 +40,12 @@ function maxOfThree(x, y, z) {
     return z;
   }
 }
-console.log(maxOfThree(2, 3, 4))
+console.log(maxOfThree(2, 3, 4));
 
 
 // 3. Define a function, as a function declaration, isCharAVowel that takes a character as an argument and returns true if it is a vowel, false otherwise.
 
-let isCharAVowel = function (character) {
+function isCharAVowel(character) {
   let vowels = ["a", "e", "i", "o", "u"];
   for (let i = 0; i < vowels.length; i++) {
     if (character === vowels[i]) {
@@ -54,7 +54,7 @@ let isCharAVowel = function (character) {
   }
   return 'false';
 }
-console.log(isCharAVowel('h'))
+console.log(isCharAVowel('h'));
 
 // 4. Define a function, as a function expression, sumArray that takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]); would return 11.
 
@@ -63,7 +63,7 @@ let sumArray = 0
 for (let i = 0; i < arr.length; i++) {
   sumArray += arr[i]
 }
-console.log(sumArray)
+console.log(sumArray);
 
 // 5. Define a function, as a function declaration, multiplyArray that takes an array of numbers and returns the product of those numbers. For example, multiplyArray([2, 4, 5]);would return 40.
 
@@ -74,13 +74,12 @@ function multiplyArray(num) {
   }
   return product;
 }
-console.log(multiplyArray([2, 4, 5]))
+console.log(multiplyArray([2, 4, 5]));
 
 // 6. Define a function, as a function expression, numArgs that returns the number of arguments passed to the function when called.
 
-let arrTwo = [14, 85, 37, 95];
-let sumArgs = arrTwo.length;
-console.log(sumArgs);
+const numsArgs = function(...arrTwo){
+  return arrTwo.length
 
 // 7. Define a function, as a function declaration, reverseString that takes a string, reverses the characters, and returns it. For example, reverseString('rockstar');would return the string "ratskcor".
 
@@ -97,15 +96,27 @@ console.log(reverseString('supermodel'));
 // 8. Define a function, as a function expression, longestStringInArray that takes an array of strings as an argument and returns the length of the longest string.
 
 let array = ["Umbrella", "Location", "Xylophone", "Sunbathe"];
-function longestStringInArray(array) {
+const longestStringInArray = function (array) {
   let longest = array[0];
   for (let i = 1; i < array.length; i++) {
     if (array[i].length > longest.length) {
       longest = array[i];
     }
   }
-  return longest;
+  return longest.length;
 }
 console.log(longestStringInArray(array));
 
 // 9. Define a function, as a function declaration, stringsLongerThan that takes an array of strings and a number as arguments, and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3);would return ["hello", "morning"].
+
+function stringsLongerThan(arrayFour, num){
+  longer = []
+  arrayFour.forEach((string)=>{
+      if(string.length > num){
+          longer.push(string)
+      }
+  })
+  return longer
+}
+}
+console.log(stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3));
