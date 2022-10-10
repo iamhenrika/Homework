c937c7fb611db884c9cd6a92d2493bf1e4a38221
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2022-10-10 17:44:12 -0400

    Updated

---
+++ 100922 - Static Methods and Properties.js
@@ -0,0 +1,14 @@
+// Practice What You Learned
+// Static Methods and Properties
+// class Governor {}
+// There is only one Governor in the state, add static methods and properties you'd expect a Governor to have.
+// log the properties and test the methods (don't instantiate)
+// Links to an external site.Inheritance
+// Your base class
+
+// Person{constructor(){}}
+// Think of three properties all people share, set them with the constructor
+// Think of three methods all people share
+// Create a PostalWorker class that inherits from person, add some methods
+// Create a Chef class that inherits from person, add some methods
+// Create 2 PostalWorkers and 2 Chefs, log them and test all their methods
