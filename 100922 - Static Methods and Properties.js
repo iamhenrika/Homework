a3b44ff9a440c5a6fcfbbf9b551b9894aa56a4b6
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2022-10-10 18:26:13 -0400

    Update 100922 - Static Methods and Properties.js

--- 100922 - Static Methods and Properties.js
+++ 100922 - Static Methods and Properties.js
@@ -3,9 +3,23 @@
 // class Governor {}
 // There is only one Governor in the state, add static methods and properties you'd expect a Governor to have.
 // log the properties and test the methods (don't instantiate)
-// Links to an external site.Inheritance
-// Your base class
 
+class Governor {
+    static age = 47;
+    static gender = "Female";
+    static race = "Black";
+    static greetingOne () { "Hello, all. Thank you for taking the time to meet with me."};
+    static problem() { return "Stasi, what's today's headache?" };
+    
+}
+
+console.log(Governor);
+console.log(Governor.greetingOne());
+console.log(Governor.problem());
+
+
+// Inheritance
+// Your base class
 // Person{constructor(){}}
 // Think of three properties all people share, set them with the constructor
 // Think of three methods all people share
